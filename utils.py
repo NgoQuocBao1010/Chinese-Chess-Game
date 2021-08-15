2fac743830763432e16c303ed19ad8459ec92a8a
Bao <totti.chelsea@gmail.com>
2021-08-15 11:28:22 +0700

    git ignore reset 2

---
+++ utils.py
@@ -0,0 +1,11 @@
+class Color:
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 255, 0)
+    YELLOW = (255, 255, 0)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    PURPLE = (128, 0, 128)
+    ORANGE = (255, 165, 0)
+    GREY = (128, 128, 128)
+    TURQUOISE = (64, 224, 208)

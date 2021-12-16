58c90e607f4112624655b5ec48c0149a92956428
Bao <totti.chelsea@gmail.com>
2021-12-16 20:08:16 +0700

    🔨 refactor: relocate image pieces

--- game/utils.py
+++ game/utils.py
@@ -10,6 +10,7 @@ WIN_HEIGHT = 700
 RED_SIDE = RED_TURN = 1
 BLUE_SIDE = BLUE_TURN = 0
 
+
 class Color:
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
@@ -24,31 +25,25 @@ class Color:
 
 
 class Font:
-    SCORE_TEXT_FONT = pygame.font.Font('./fonts/CursedTimerUlil-Aznm.ttf', 30)
-    SCORE_FONT = pygame.font.Font('./fonts/CursedTimerUlil-Aznm.ttf', 30, bold=True)
-    NORMAL_FONT = pygame.font.Font('./fonts/Poppins-Bold.ttf', 30)
-    WRITING_FONT = pygame.font.Font('./fonts/Allison-Regular.ttf', 30)
+    SCORE_TEXT_FONT = pygame.font.Font("./fonts/CursedTimerUlil-Aznm.ttf", 30)
+    SCORE_FONT = pygame.font.Font("./fonts/CursedTimerUlil-Aznm.ttf", 30, bold=True)
+    NORMAL_FONT = pygame.font.Font("./fonts/Poppins-Bold.ttf", 30)
+    WRITING_FONT = pygame.font.Font("./fonts/Allison-Regular.ttf", 30)
 
 
 class ChessImages:
-    RED_CHARIOT = pygame.image.load("./images/red-car.png")
-    RED_CANNON = pygame.image.load("./images/red-cannon.png")
-    RED_HORSE = pygame.image.load("./images/red-horse.png")
-    RED_ELEPHANT = pygame.image.load("./images/red-elephant.png")
-    RED_SOLDIER = pygame.image.load("./images/red-pawn.png")
-    RED_ADVISOR = pygame.image.load("./images/red-bodyguard.png")
-    RED_LORD = pygame.image.load("./images/red-king.png")
+    RED_CHARIOT = pygame.image.load("./images/pieces/red-car.png")
+    RED_CANNON = pygame.image.load("./images/pieces/red-cannon.png")
+    RED_HORSE = pygame.image.load("./images/pieces/red-horse.png")
+    RED_ELEPHANT = pygame.image.load("./images/pieces/red-elephant.png")
+    RED_SOLDIER = pygame.image.load("./images/pieces/red-pawn.png")
+    RED_ADVISOR = pygame.image.load("./images/pieces/red-bodyguard.png")
+    RED_LORD = pygame.image.load("./images/pieces/red-king.png")
 
-    BLUE_CHARIOT = pygame.image.load("./images/blue-car.png")
-    BLUE_CANNON = pygame.image.load("./images/blue-cannon.png")
-    BLUE_HORSE = pygame.image.load("./images/blue-horse.png")
-    BLUE_ELEPHANT = pygame.image.load("./images/blue-elephant.png")
-    BLUE_SOLDIER = pygame.image.load("./images/blue-pawn.png")
-    BLUE_ADVISOR = pygame.image.load("./images/blue-bodyguard.png")
-    BLUE_LORD = pygame.image.load("./images/blue-king.png")
-        
-        
-        
-
-
-
+    BLUE_CHARIOT = pygame.image.load("./images/pieces/blue-car.png")
+    BLUE_CANNON = pygame.image.load("./images/pieces/blue-cannon.png")
+    BLUE_HORSE = pygame.image.load("./images/pieces/blue-horse.png")
+    BLUE_ELEPHANT = pygame.image.load("./images/pieces/blue-elephant.png")
+    BLUE_SOLDIER = pygame.image.load("./images/pieces/blue-pawn.png")
+    BLUE_ADVISOR = pygame.image.load("./images/pieces/blue-bodyguard.png")
+    BLUE_LORD = pygame.image.load("./images/pieces/blue-king.png")
